ca5d5d090f6914bd0f796811c02ef464815db969
Piyush Sinha <piyushsinha0526@gmail.com>
2024-09-05 12:13:54 +0530

    feat: added signup & login controllers

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,10 +1,34 @@
-function signup(req, res) {
-  // TODO: implement signup
-}
+import { compare } from "bcrypt";
+import { User } from "../models/user.js";
+import sendToken from "../utils/jwtToken.js";
+import { exceptionHandler } from "../middlewares/error.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
-function login(req, res) {
-  // TODO: implement login
-}
+const signup = exceptionHandler(async (req, res) => {
+  console.log('singup ----', req.body)
+  const { name, userName, password } = req.body;
+  // TODO: profilePic
+
+  const user = await User.create({ name, userName, password });
+  sendToken(res, user, 201, "User Created Successfully");
+});
+
+const login = exceptionHandler(async (req, res, next) => {
+  console.log('login ----', req.body)
+  const { userName, password } = req.body;
+
+  const user = await User.findOne({ userName }).select("+password").lean();
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  const isMatch = await compare(password, user.password);
+  if (!isMatch) {
+    return next(new ErrorHandler("Incorrect Password", 401));
+  }
+  delete user.password;
+  sendToken(res, user, 200, "User logged in Successfully");
+});
 
 function getProfile(req, res) {
   // TODO: implement getProfile
@@ -33,3 +57,15 @@ function getMyNotifications(req, res) {
 function getMyFriends(req, res) {
   // TODO: implement getMyFriends
 }
+
+export {
+  signup,
+  login,
+  getProfile,
+  searchUser,
+  logout,
+  sendFriendRequest,
+  acceptFriendRequest,
+  getMyNotifications,
+  getMyFriends,
+};
