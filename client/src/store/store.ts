9868a7ad98502aa446115e755e4f553a0a265449
Piyush Sinha <piyushsinha0526@gmail.com>
2024-10-11 00:29:57 +0530

    added: user slice  and api store config.

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,21 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userApi from "./api/userApi";
 import authSlice from "./slices/authSlice";
 import authApi from "./api/authApi";
 import chatSlice from "./slices/chatSlice";
+import miscSlice from "./slices/miscSlice";
 import chatApi from "./api/chatApi";
 
 export const store = configureStore({
   reducer: {
     // api(s)
     [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
     [chatApi.reducerPath]: chatApi.reducer,
 
     // slices
     [authSlice.name]: authSlice.reducer,
     [chatSlice.name]: chatSlice.reducer,
+    [miscSlice.name]: miscSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, chatApi.middleware),
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      userApi.middleware,
+      chatApi.middleware
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
