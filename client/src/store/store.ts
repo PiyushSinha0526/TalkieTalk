87c817814949d3dc36bc5734161d76c569e37e07
Piyush Sinha <piyushsinha0526@gmail.com>
2024-10-10 21:47:23 +0530

    added: chat slice and api store config.

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,16 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
 import authApi from "./api/authApi";
+import chatSlice from "./slices/chatSlice";
+import chatApi from "./api/chatApi";
 
 export const store = configureStore({
   reducer: {
     // api(s)
     [authApi.reducerPath]: authApi.reducer,
+    [chatApi.reducerPath]: chatApi.reducer,
+
     // slices
     [authSlice.name]: authSlice.reducer,
+    [chatSlice.name]: chatSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, chatApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
