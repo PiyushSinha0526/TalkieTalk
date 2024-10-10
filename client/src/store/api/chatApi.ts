68a78f61ebad2249c1ae894b9def0246d3c304e1
Piyush Sinha <piyushsinha0526@gmail.com>
2024-10-10 21:42:29 +0530

    added: chat slice and api

---
+++ client/src/store/api/chatApi.ts
@@ -0,0 +1,133 @@
+import { serverUrl } from "@/constants";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQueryWithToken = fetchBaseQuery({
+  baseUrl: `${serverUrl}/chat`,
+  credentials: "include",
+});
+
+const chatApi = createApi({
+  reducerPath: "chatApi",
+  baseQuery: baseQueryWithToken,
+  tagTypes: ["Chat", "Message", "ChatList"],
+
+  endpoints: (builder) => ({
+    myChats: builder.query({
+      query: () => ({
+        url: "/myChats",
+        credentials: "include",
+      }),
+      providesTags: ["ChatList"],
+    }),
+
+    chatDetails: builder.query({
+      query: ({ chatId, populate = false }) => {
+        let url = `/${chatId}`;
+        if (populate) url += "?populate=true";
+        return {
+          url,
+          credentials: "include",
+        };
+      },
+      providesTags: ({ chatId }) => [{ type: "Chat", id: chatId }],
+    }),
+
+    getMessages: builder.query({
+      query: ({ chatId, page }) => ({
+        url: `/message/${chatId}?page=${page}`,
+        credentials: "include",
+      }),
+      keepUnusedDataFor: 0,
+    }),
+
+    sendAttachments: builder.mutation({
+      query: (data) => ({
+        url: "/message",
+        method: "POST",
+        credentials: "include",
+        body: data,
+      }),
+    }),
+
+    myGroups: builder.query({
+      query: () => ({
+        url: "/my/groups",
+        credentials: "include",
+      }),
+      providesTags: ["ChatList"],
+    }),
+
+    newGroup: builder.mutation({
+      query: (data) => ({
+        url: "/new",
+        method: "POST",
+        credentials: "include",
+        body: data,
+      }),
+      invalidatesTags: ["ChatList"],
+    }),
+
+    renameGroup: builder.mutation({
+      query: ({ chatId, name }) => ({
+        url: `/${chatId}`,
+        method: "PUT",
+        credentials: "include",
+        body: { name },
+      }),
+      invalidatesTags: ({ chatId }) => [{ type: "Chat", id: chatId }],
+    }),
+
+    removeGroupMember: builder.mutation({
+      query: ({ chatId, userId }) => ({
+        url: `/removemember`,
+        method: "DELETE",
+        credentials: "include",
+        body: { chatId, userId },
+      }),
+      invalidatesTags: ({ chatId }) => [{ type: "Chat", id: chatId }],
+    }),
+
+    addGroupMembers: builder.mutation({
+      query: ({ members, chatId }) => ({
+        url: `/addmembers`,
+        method: "PUT",
+        credentials: "include",
+        body: { members, chatId },
+      }),
+      invalidatesTags: ({ chatId }) => [{ type: "Chat", id: chatId }],
+    }),
+
+    deleteChat: builder.mutation({
+      query: (chatId) => ({
+        url: `/${chatId}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+      invalidatesTags: ({ chatId }) => [{ type: "Chat", id: chatId }],
+    }),
+
+    leaveGroup: builder.mutation({
+      query: (chatId) => ({
+        url: `/leave/${chatId}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+      invalidatesTags: ({ chatId }) => [{ type: "Chat", id: chatId }],
+    }),
+  }),
+});
+
+export default chatApi;
+export const {
+  useMyChatsQuery,
+  useChatDetailsQuery,
+  useGetMessagesQuery,
+  useSendAttachmentsMutation,
+  useMyGroupsQuery,
+  useNewGroupMutation,
+  useRenameGroupMutation,
+  useRemoveGroupMemberMutation,
+  useAddGroupMembersMutation,
+  useDeleteChatMutation,
+  useLeaveGroupMutation,
+} = chatApi;
---
+++ client/src/store/slices/chatSlice.ts
@@ -0,0 +1,62 @@
+import { NEW_MESSAGE_ALERT } from "@/constants/socketEvents";
+import { getLocalValue } from "@/utils/localStorage";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  selectedChatItem: {
+    _id: "",
+    groupChat: false,
+    profilePic: [],
+    name: "",
+    creater: "",
+    members: [],
+  },
+  notificationCount: 0,
+  newMessagesAlert: getLocalValue(NEW_MESSAGE_ALERT) || [
+    {
+      chatId: "",
+      count: 0,
+    }
+  ],
+};
+const chatSlice = createSlice({
+  name: "chat",
+  initialState,
+  reducers: {
+    setSelectedChatItem(state, action) {
+      state.selectedChatItem = action.payload;
+    },
+    incrementNotificationCount(state) {
+      state.notificationCount += 1;
+    },
+    resetNotificationCount(state) {
+      state.notificationCount = 0;
+    },
+    setNewMessagesAlert(state, action) {
+      const index = state.newMessagesAlert.findIndex(
+        (item: any) => item.chatId === action.payload.chatId
+      );
+
+      if (index !== -1) {
+        state.newMessagesAlert[index].count +=1;
+      } else {
+        state.newMessagesAlert.push({
+          chatId: action.payload.chatId,
+          count: 1,
+        });
+      }
+    },
+    removeNewMessagesAlert(state, action) {
+      state.newMessagesAlert = state.newMessagesAlert.filter((item: any) => item.chatId !== action.payload);
+    }
+  },
+});
+
+export const {
+  setSelectedChatItem,
+  incrementNotificationCount,
+  resetNotificationCount,
+  setNewMessagesAlert,
+  removeNewMessagesAlert,
+} = chatSlice.actions;
+export default chatSlice;
