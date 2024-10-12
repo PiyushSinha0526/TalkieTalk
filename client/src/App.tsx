fd02ea5847b3e66521a64ff89c013a5034df40a6
Piyush Sinha <piyushsinha0526@gmail.com>
2024-10-12 11:09:34 +0530

    feat: signup login page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,57 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy, Suspense, useEffect } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "./hooks";
+import { useGetProfileQuery } from "./store/api/authApi";
+import { clearUser, setUser } from "./store/slices/authSlice";
 
 const Cred = lazy(() => import("./pages/Cred"));
 const Chat = lazy(() => import("./pages/Chat"));
 
+function PrivateRoute({
+  children,
+  user,
+}: {
+  children: JSX.Element;
+  user: boolean;
+}) {
+  return user ? children : <Navigate to="/" />;
+}
+
 function App() {
+  const dispatch = useAppDispatch();
+  const { data: profileData, isSuccess, isError } = useGetProfileQuery("");
+  const { userAuth, loading } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isSuccess && profileData) {
+      dispatch(setUser(profileData.user));
+    } else if (isError) {
+      dispatch(clearUser());
+    }
+  }, [isSuccess, profileData, dispatch, isError]);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
     <div className="h-screen w-screen">
-      <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Cred />} />
-            <Route path="/chat" element={<Chat />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route
+                path="/"
+                element={userAuth ? <Navigate to="/chat" /> : <Cred />}
+              />
+              <Route
+                path="/chat"
+                element={
+                  <PrivateRoute user={!!userAuth}>
+                    <Chat />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
     </div>
   );
 }
