273c843fe54fb8d8db1efafffa6d917d63c89610
Piyush Sinha <piyushsinha0526@gmail.com>
2024-10-12 09:21:15 +0530

    setting up basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
-function App() {
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+const Cred = lazy(() => import("./pages/Cred"));
+const Chat = lazy(() => import("./pages/Chat"));
+
+function App() {
   return (
-    <>
-    Hello
-    </>
-  )
+    <div className="h-screen w-screen">
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Cred />} />
+            <Route path="/chat" element={<Chat />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Cred.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cred = () => {
+  return (
+    <div>Cred</div>
+  )
+}
+
+export default Cred
